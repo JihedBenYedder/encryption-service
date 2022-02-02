efa8dfd676cc538c29b5dc09a087f71d830623ad
Ben <ben@alpha.com>
2022-02-02 14:57:54 +0100

    fix

--- src/docker/entrypoint.sh
+++ src/docker/entrypoint.sh
@@ -1,7 +1,7 @@
-#!/usr/bin/env bash
-set -e
+#!/bin/bash
 
-java -Djavax.net.ssl.trustStore=truststore.jks -Djavax.net.ssl.truststoretype=JKS -Djavax.net.ssl.trustStorePassword=changeit -Dserver.port=8080 ${JAVA_OPTS} -jar ccp-app.jar
+ls -l
+java  ${JAVA_OPTS} -jar app.jar
 
 #exec "$@"
 
