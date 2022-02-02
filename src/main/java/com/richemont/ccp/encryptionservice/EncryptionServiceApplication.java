70fc366e544f47cafd9907fd1624d4d6f8d9fd90
Ben <ben@alpha.com>
2022-02-02 20:12:51 +0100

    fix

--- src/main/java/com/richemont/ccp/encryptionservice/EncryptionServiceApplication.java
+++ src/main/java/com/richemont/ccp/encryptionservice/EncryptionServiceApplication.java
@@ -1,13 +1,25 @@
 package com.richemont.ccp.encryptionservice;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.vault.authentication.SessionManager;
+
+import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class EncryptionServiceApplication {
 
+	@Autowired
+	private SessionManager sessionManager;
+
 	public static void main(String[] args) {
 		SpringApplication.run(EncryptionServiceApplication.class, args);
+	}
+
+	@PostConstruct
+	public void initIt() throws Exception {
+		System.out.println("Got Vault Token: " + sessionManager.getSessionToken().getToken());
 	}
 
 }
