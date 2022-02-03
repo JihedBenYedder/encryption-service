3d45eea47b7763536ea7e51580d3aef1844d975d
Ben <ben@alpha.com>
2022-02-03 01:24:46 +0100

    fix

--- src/main/java/com/richemont/ccp/encryptionservice/EncryptionServiceAPIController.java
+++ src/main/java/com/richemont/ccp/encryptionservice/EncryptionServiceAPIController.java
@@ -1,11 +1,16 @@
 package com.richemont.ccp.encryptionservice;
 
 import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Iterator;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
@@ -15,14 +20,30 @@ public class EncryptionServiceAPIController {
 	private EncryptionService encryptionService;
 
 	@RequestMapping(path = "/encrypt", method = RequestMethod.POST)
-	public ResponseEntity<String> encrypt(final String str)  {
-			String encryptedVal = encryptionService.encrypt(str);
-		return ResponseEntity.ok(encryptedVal);
+	public ResponseEntity<String> encrypt(final @RequestBody String str) throws JSONException {
+		JSONObject jsonpObject = new JSONObject(str);
+		Iterator<String> keys = jsonpObject.keys();
+		JSONObject returnObject = new JSONObject();
+		while (keys.hasNext()) {
+			String key = keys.next();
+			String encryptedVal = encryptionService.encrypt(jsonpObject.get(key).toString());
+            String encryptedKey = encryptionService.encrypt(key);
+            returnObject.put(encryptedKey, encryptedVal);
+		}
+		return ResponseEntity.ok(returnObject.toString());
 	}
 
 	@RequestMapping(path = "/decrypt", method = RequestMethod.POST)
-	public ResponseEntity<String> decrypt(final String str)  {
-            String decryptedKey = encryptionService.decrypt(str);
-		return ResponseEntity.ok(decryptedKey);
+	public ResponseEntity<String> decrypt(final @RequestBody String str) throws JSONException {
+		JSONObject jsonpObject = new JSONObject(str);
+		Iterator<String> keys = jsonpObject.keys();
+		JSONObject returnObject = new JSONObject();
+		while (keys.hasNext()) {
+			String encryptedkey = keys.next();
+            String decryptedKey = encryptionService.decrypt(encryptedkey);
+			String val = encryptionService.decrypt(jsonpObject.get(encryptedkey).toString());
+			returnObject.put(decryptedKey, val);
+		}
+		return ResponseEntity.ok(returnObject.toString());
 	}
 }
