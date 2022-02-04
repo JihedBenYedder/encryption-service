bbeba1fdaedc20c1dd8494359e848ebef07f3f9d
Ben <ben@alpha.com>
2022-02-04 16:24:23 +0100

    fix

--- src/main/java/com/richemont/ccp/encryptionservice/EncryptionServiceApplication.java
+++ src/main/java/com/richemont/ccp/encryptionservice/EncryptionServiceApplication.java
@@ -1,12 +1,8 @@
 package com.richemont.ccp.encryptionservice;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
-import org.springframework.vault.authentication.SessionManager;
 
-import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class EncryptionServiceApplication {
